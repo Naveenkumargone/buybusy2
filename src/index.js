a09c0dfbee0d37fdcd1b33262266a1e8ad10dece
Naveenkumar Gone <naveenkumargone1234@gmail.com>
2024-10-24 10:41:30 +0530

    application build created

--- src/index.js
+++ src/index.js
@@ -10,14 +10,14 @@ import { getAuth } from 'firebase/auth';
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Initialize Firebase with environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyAFG5D0fCyDJj3zFBkuOxPmbhcEBB9E_cA",
-  authDomain: "buybusy2-be5d4.firebaseapp.com",
-  projectId: "buybusy2-be5d4",
-  storageBucket: "buybusy2-be5d4.appspot.com",
-  messagingSenderId: "364521705169",
-  appId: "1:364521705169:web:7cda0f16b5840c99b47ff0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
